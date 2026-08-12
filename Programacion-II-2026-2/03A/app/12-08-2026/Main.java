9dabf89a439efdba4b8d4349999a8bf8bebcf175
carlosandrestamayo <carlosandrestamayo1285@gmail.com>
2026-08-12 10:39:33 -0500

    prueba git

--- Programacion-II-2026-2/03A/app/12-08-2026/Main.java
+++ Programacion-II-2026-2/03A/app/12-08-2026/Main.java
@@ -2,5 +2,29 @@ public class Main{
     public static void main(String [] args){
       // Mièrcoles 12 de Agosto de 2026 10:00 am
     
+        //While in Python
+        //n = 1
+        //while(n <= 5):
+        //  print("n: ", n)
+        //  n = n + 1;
+        //While in Java
+        
+        System.out.println("While in Java\n");
+        int n = 1;
+        while(n <= 5){
+            System.out.println("n: " + n);
+            n++;
+        }
+
+        //For in Python
+        // for i in range(1, 11, 1):
+        //     print("i: ", i)
+
+        System.out.println("\nFor in Java\n");
+        
+        for(int i = 1; i < 11; i = i + 1 ){
+            System.out.println("i: " + i);
+        }
+
     }
 }
